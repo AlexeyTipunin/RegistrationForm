a57cd6b61033c8db5bdeb3d063c541fff52bcad3
alexey.tipunin <alexey.tipunin@gmail.com>
2019-02-07 20:07:04 +0300

    no message

---
+++ RegistrationForm/ClientApp/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { RegistrationStep2Component } from './registration-step2/registration-step2.component';
+import { RegistrationStep1Component } from './registration-step1/registration-step1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    RegistrationStep2Component,
+    RegistrationStep1Component
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/Step1', pathMatch: 'full' },
+      { path: 'Step1', component: RegistrationStep1Component, pathMatch: 'full' },
+      { path: 'Step2', component: RegistrationStep2Component, pathMatch: 'full' },
+      // { path: '', component: HomeComponent, pathMatch: 'full' },
+      // { path: 'counter', component: CounterComponent },
+      // { path: 'fetch-data', component: FetchDataComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

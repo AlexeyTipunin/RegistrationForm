49715aa1880edccbd8f0a80c87a2276bef192a5a
alexey.tipunin <alexey.tipunin@gmail.com>
2019-02-11 10:21:48 +0300

    no message

--- RegistrationForm/ClientApp/src/app/registration-step2/registration-step2.component.ts
+++ RegistrationForm/ClientApp/src/app/registration-step2/registration-step2.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { WizardService } from '../wizard.service';
 import { PasswordComplexityCheckerService } from '../password-complexity-checker.service';
 import { Router } from '@angular/router';
 import { AccountWithPassword } from '../models/accountWithPassword';
+import { RegistrationFormValidator } from '../misc/RegistrationFormValidator';
+import { Country } from '../models/Country';
+import { Province } from '../models/province';
 
 @Component({
   selector: 'app-registration-step2',
@@ -13,6 +16,14 @@ import { AccountWithPassword } from '../models/accountWithPassword';
 export class RegistrationStep2Component implements OnInit {
 
   account: AccountWithPassword;
+  form: FormGroup;
+
+  countryList: Country[];
+  selectedCountry: Country;
+
+  provinceList: Province[];
+  selectedProvince: Province;
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -22,9 +33,32 @@ export class RegistrationStep2Component implements OnInit {
   ) { }
 
   ngOnInit() {
-    let account = this.wizardService.getAccount();
-    if(!account){
-      this.router.navigate(['/step1']);
+    // let account = this.wizardService.getAccount();
+    // if(!account){
+    //   this.router.navigate(['/step1']);
+    // }
+
+    this.form = this.formBuilder.group({
+      'country': new FormControl(this.account.login, [
+        Validators.required,
+        // Validators.email
+      ]),
+      'Province': new FormControl(this.account.login, [
+        Validators.required,
+        RegistrationFormValidator.passwordComplexityChecker(this.passwordComplexityChecker)
+      ])});
+  }
+
+  get login() { return this.form.get('login'); }
+  get password() { return this.form.get('password'); }
+  get passwordConfirmation() { return this.form.get('passwordConfirmation'); }
+  get agreement() { return this.form.get('agreement'); }
+
+  onSubmit() {
+    if(this.form.valid)
+    {
+      //this.wizardService.setAccount(this.account)
+      this.router.navigate(['/accountsList']);
     }
   }
 
